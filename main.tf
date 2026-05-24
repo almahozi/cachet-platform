17d1dc714ba188857184a9db73b45953b960dd60
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-24 15:08:59 +0300

    Call compute module.

--- main.tf
+++ main.tf
@@ -3,8 +3,15 @@ module "networking" {
 }
 
 module "database" {
-  source = "./modules/database"
-  private_subnet_ids = module.networking.private_subnet_ids
+  source               = "./modules/database"
+  private_subnet_ids   = module.networking.private_subnet_ids
   db_security_group_id = module.networking.db_security_group_id
-  db_password = var.db_password
+  db_password          = var.db_password
+}
+
+module "compute" {
+  source            = "./modules/compute"
+  subnet_id         = module.networking.public_subnet_id[0]
+  security_group_id = module.networking.security_group_id
+  db_endpoint       = module.database.db_endpoint
 }
