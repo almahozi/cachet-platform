615af065f6fac4b225c30e3663288410068a4a20
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-24 14:26:40 +0300

    Call database module.

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 module "networking" {
   source = "./modules/networking"
 }
+
+module "database" {
+  source = "./modules/database"
+  private_subnet_ids = module.networking.private_subnet_ids
+  db_security_group_id = module.networking.db_security_group_id
+  db_password = var.db_password
+}
