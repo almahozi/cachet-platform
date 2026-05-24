098f1bea92346c761a3de2f95ccef6f5cb1fbfcc
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-24 16:32:15 +0300

    Add AZ's to private subnets to meet min req. of 2 AZ's.

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -28,6 +28,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "private_1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
     Name = "cachet-private-1"
@@ -37,6 +38,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
     Name = "cachet-private-2"
