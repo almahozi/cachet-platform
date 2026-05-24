b27a9f1dfb01ce3ae71a29ccedf650279785cdbb
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-24 11:18:09 +0300

    Output private subnets ID's

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -9,3 +9,7 @@ output "public_subnet_id" {
 output "security_group_id" {
   value = aws_security_group.cachet_sg.id
 }
+
+output "private_subnet_ids" {
+  value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
