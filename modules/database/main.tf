509209b162622878d2f5756ef6b0e562ee3a1c06
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-24 16:32:51 +0300

    Remove subnet group resource as  it's already defined in the networking module.

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -8,14 +8,9 @@ resource "aws_db_instance" "cachet_db" {
   password          = var.db_password
 
   # Networking
-  db_subnet_group_name   = aws_db_subnet_group.db_group.name
+  db_subnet_group_name   = var.db_subnet_group_name
   vpc_security_group_ids = [var.db_security_group_id]
 
   skip_final_snapshot = true
   publicly_accessible = false
-}
-
-resource "aws_db_subnet_group" "db_group" {
-  name       = "cachet-db-subnet-group"
-  subnet_ids = var.private_subnet_ids
 }
