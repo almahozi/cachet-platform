fa59eaebec400ad5e8edc442bc7b34bcba52e376
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-24 16:39:45 +0300

    Output db subnet group name.

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -14,6 +14,11 @@ output "private_subnet_ids" {
   value = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 }
 
+output "db_subnet_group_name" {
+  value       = aws_db_subnet_group.db_group.name
+  description = "The name of the database subnet group created by networking"
+}
+
 output "db_security_group_id" {
   value       = aws_security_group.db_sg.id
   description = "The ID of the RDS security group"
