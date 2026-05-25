87fadca8cc4aef84a5c4611440f63e78916956d7
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-25 20:13:59 +0300

    Add gateway to make the public subnet actually public.

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -23,6 +23,24 @@ resource "aws_subnet" "public_1" {
     }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
 # Private subnets
 
 resource "aws_subnet" "private_1" {
