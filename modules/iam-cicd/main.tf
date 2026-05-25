46be47cd7ae5ad1308a747baff0b7462e18defa7
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2026-05-25 15:42:17 +0300

    Allow describe instances for github actions role.

--- modules/iam-cicd/main.tf
+++ modules/iam-cicd/main.tf
@@ -11,16 +11,24 @@ resource "aws_iam_role" "github_actions" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
-      Action    = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringLike = {
-          "token.actions.githubusercontent.com:sub": "repo:almahozi/cachet-platform:*"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+        Action    = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub": "repo:almahozi/cachet-platform:*"
+          }
         }
+      },
+
+      {
+        Effect   = "Allow"
+        Action   = "ec2:DescribeInstances"
+        Resource = "*"
       }
-    }]
+    ]
   })
 }
 
